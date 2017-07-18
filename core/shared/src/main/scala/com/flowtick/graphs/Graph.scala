dcad16148b3f1cdb4d4f3a18a7040b7365021d9d
adrobisch <andreas@drobisch.com>
2017-07-18 23:25:27 +0200

    (chore): formatting

--- core/shared/src/main/scala/com/flowtick/graphs/Graph.scala
+++ core/shared/src/main/scala/com/flowtick/graphs/Graph.scala
@@ -29,8 +29,7 @@ trait Graph[N <: Node, E <: Edge[N]] {
 }
 
 object Graph {
-  def create[N <: Node, E <: Edge[N]](block: GraphBuilder[N, E] => Unit)
-                                     (implicit graphBuilderProvider: () => GraphBuilder[N, E]): Graph[N, E] =
+  def create[N <: Node, E <: Edge[N]](block: GraphBuilder[N, E] => Unit)(implicit graphBuilderProvider: () => GraphBuilder[N, E]): Graph[N, E] =
     graphBuilderProvider()(block)
 }
 
