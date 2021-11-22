4f73f2adaba774d8112b8509abef6922056a6bc9
Andreas Drobisch <github@drobisch.com>
2021-11-22 18:59:47 +0100

    (view): fix test

--- view/jvm/src/test/scala/com/flowtick/graphs/view/SVGRendererJvmSpec.scala
+++ view/jvm/src/test/scala/com/flowtick/graphs/view/SVGRendererJvmSpec.scala
@@ -1,22 +1,19 @@
 package com.flowtick.graphs.view
 
 import cats.effect.IO
-import cats.effect.testing.scalatest.AsyncIOSpec
+import cats.effect.unsafe.implicits.global
 import com.flowtick.graphs.Graph
 import com.flowtick.graphs.defaults._
 import com.flowtick.graphs.defaults.label._
 import com.flowtick.graphs.layout.ForceDirectedLayout
-
 import com.flowtick.graphs.style.StyleSheet
 import com.flowtick.graphs.style.defaults._
-
-import org.scalatest.AsyncTestSuite
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 import java.io.FileOutputStream
 
-class SVGRendererJvmSpec extends AnyFlatSpec with Matchers with AsyncTestSuite with AsyncIOSpec {
+class SVGRendererJvmSpec extends AnyFlatSpec with Matchers {
   "Editor Renderer" should "render node" in {
     val graph: Graph[Unit, String] = Graph.fromEdges(Set("A" --> "B"))
 
@@ -25,8 +22,10 @@ class SVGRendererJvmSpec extends AnyFlatSpec with Matchers with AsyncTestSuite w
 
     val renderedSvg = for {
       layout <- IO.fromFuture(IO(ForceDirectedLayout.layout(graph)))
-      _ <- renderer.renderGraph(ViewContext(graph, layout, styleSheet))
-      xmlString <- IO.fromTry(renderer.toXmlString)
+      xmlString <- renderer
+        .translateAndScaleView(0, 0, 2)
+        .renderGraph(ViewContext(graph, layout, styleSheet))
+        .flatMap(_.toXmlString)
       _ <- IO {
         val fileOut = new FileOutputStream("target/test_simple.svg")
         fileOut.write(xmlString.getBytes("UTF-8"))
@@ -35,6 +34,6 @@ class SVGRendererJvmSpec extends AnyFlatSpec with Matchers with AsyncTestSuite w
       }
     } yield renderer.graphSVG
 
-    renderedSvg.assertNoException
+    renderedSvg.unsafeRunSync()
   }
 }
